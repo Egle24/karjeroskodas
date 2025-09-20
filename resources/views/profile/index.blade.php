a8bd35028916dbd1d318063ee39c0586d1c6a0f5
Egle <126772981+Egle24@users.noreply.github.com>
2025-09-20 14:04:59 +0300

    Update index.blade.php

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -52,7 +52,7 @@
                             
                            
                                 <h5 class="card-title">Profilio informacija</h5>
-                                @if(auth()->user()->role === 'admin') 
+                                @if(auth()->user()->roles->contains('name', 'admin')) 
                                 <form action="{{ route('profile.updateName') }}" method="POST" class="mb-3 d-flex align-items-center gap-2">
                                     @csrf
                                     <div class="row g-2 align-items-center">
